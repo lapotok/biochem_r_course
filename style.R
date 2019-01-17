11ef1abb9b611ca596ae011aa210c84cbb6ae285
lapotok <lapotok@gmail.com>
2019-01-17 18:59:40 +0300

    html templates

---
+++ style.R
@@ -0,0 +1,8 @@
+knitr::opts_chunk$set(fig.align  = "center", 
+                      fig.width  = 4, 
+                      fig.height = 4, 
+                      echo       = T, 
+                      eval       = F, 
+                      comment    = "", 
+                      warning    = F,
+                      message    = F)
